d1af76b46d8272c3a0c44fa63239dc2a9ba997e8
Shamik Palit <shamik1980@gmail.com>
2026-03-16 09:40:00 +0400

    Create Main.java

---
+++ src/com/exam/Main.java
@@ -0,0 +1,21 @@
+package com.exam;
+
+import com.exam.controller.StudentController;
+import com.exam.model.Student;
+import com.exam.view.StudentView;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(view);
+
+        controller.addStudent(new Student(101, "Amit", "Mathematics", 78));
+        controller.addStudent(new Student(102, "Riya", "Physics", 35));
+        controller.addStudent(new Student(103, "Sohan", "Chemistry", 64));
+
+        controller.displayAllStudents();
+        controller.displayStudent(102);
+        controller.updateMarks(102, 55);
+        controller.displayStudent(102);
+    }
+}
